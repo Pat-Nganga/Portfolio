38887c4fad25849414404115062f014e5634f5ed
Wambui Patricia <wambuipatricia3@gmail.com>
2023-10-04 21:22:45 +0300

    Added fadeIn

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import './Home.css'
-import bg  from'../assets/photobck.png'
+// import React from 'react';
+// import './Home.css'
+// import bg  from'../assets/photobck.png'
 
 
-const Home = () => {
-  return (
-    <div>
+// const Home = () => {
+//   return (
+//     <div>
 
-    <section id="intro">
-        <div className='introContent'>
-            <span className='hello'>Hello,</span>
-            <span className='hello'>I'm <span className='introName'>Patricia Ng'ang'a</span> <br/>Software Engineer</span>
-            <p className='introPara'>I'm a passionate software engineer, skilled in solving complex problems through code.</p>
+//     <section id="intro">
+//         <div className='introContent'>
+//             <span className='hello'>Hello,</span>
+//             <span className='hello'>I'm <span className='introName'>Patricia Ng'ang'a</span> <br/>A Software Engineer</span>
+//             <p className='introPara'>I'm a passionate software engineer, skilled in solving complex problems through code.</p>
            
 
+//         </div>
+//         <img src={bg} alt="Profile" className='bg'/>
+//     </section>
+//     </div>
+
+//   )
+// }
+
+// export default Home;
+import React, { useEffect } from 'react';
+import './Home.css';
+import bg from '../assets/photobck.png';
+
+const Home = () => {
+  useEffect(() => {
+    const introContent = document.querySelector('.introContent');
+    introContent.classList.add('animate-fade-in');
+  }, []);
+
+  return (
+    <div>
+      <section id="intro">
+        <div className='introContent'>
+        <span className='hello'>Hello,</span>
+            <span className='hello'>I'm <span className='introName'>Patricia Ng'ang'a</span> <br/>A Software Engineer</span>
+            <p className='introPara'>I'm a passionate software engineer, skilled in solving complex problems through code.</p>
         </div>
         <img src={bg} alt="Profile" className='bg'/>
-    </section>
+      </section>
     </div>
-
-  )
+  );
 }
 
 export default Home;
